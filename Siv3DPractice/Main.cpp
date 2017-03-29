f116d8c92961298ed8acf1be57afe16e8abc5303
Lee Kwenwon <highfence@naver.com>
2017-03-29 14:35:17 +0900

    Login 화면 구성중

---
+++ Siv3DPractice/Main.cpp
@@ -0,0 +1,27 @@
+﻿
+# include <Siv3D.hpp>
+
+void Main()
+{
+	Graphics::SetBackground(Color(160, 200, 100));
+
+	GUI loginGui(GUIStyle::Default);
+	loginGui.setTitle(L"Login");
+
+	// 버튼 구성.
+	loginGui.add(L"loginBtn", GUIButton::Create(L"LOGIN"));
+	loginGui.add(L"cancelBtn", GUIButton::Create(L"EXIT"));
+
+	// 창 위치 지정.
+	loginGui.setCenter(Window::Center());
+
+	while (System::Update())
+	{
+		if (loginGui.button(L"cancelBtn").pushed)
+		{
+			System::Exit();
+		}
+	}
+
+	return;
+}
